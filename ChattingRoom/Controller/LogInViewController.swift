838da6fbfbcae07efce5b320af4c0f03cf76a17e
medhat <medhat.mebed@gmail.com>
2018-02-15 06:11:15 +0200

    Finish

--- ChattingRoom/Controller/LogInViewController.swift
+++ ChattingRoom/Controller/LogInViewController.swift
@@ -7,11 +7,11 @@
 //
 
 import UIKit
-
+import Firebase
+import SVProgressHUD
 
 class LogInViewController: UIViewController {
 
-    //Textfields pre-linked with IBOutlets
     @IBOutlet var emailTextfield: UITextField!
     @IBOutlet var passwordTextfield: UITextField!
     
@@ -27,10 +27,29 @@ class LogInViewController: UIViewController {
    
     @IBAction func logInPressed(_ sender: AnyObject) {
 
-        
+        SVProgressHUD.show()
         //TODO: Log in the user
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            if error != nil {
+                self.failureAlert()
+                print(error!)
+            }
+            else {
+                print("login is success")
+                SVProgressHUD.dismiss()
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
         
-        
+    }
+    func failureAlert(){
+        let alert = UIAlertController(title: "login failed", message: "invalid username or password", preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: { (action) in
+            SVProgressHUD.dismiss()
+            self.dismiss(animated: true, completion: nil)
+            })
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
     }
     
 
--- ChattingRoom/Controller/RegisterViewController.swift
+++ ChattingRoom/Controller/RegisterViewController.swift
@@ -7,12 +7,11 @@
 //
 
 import UIKit
-
+import Firebase
+import SVProgressHUD
 
 class RegisterViewController: UIViewController {
 
-    
-    //Pre-linked IBOutlets
 
     @IBOutlet var emailTextfield: UITextField!
     @IBOutlet var passwordTextfield: UITextField!
@@ -31,14 +30,31 @@ class RegisterViewController: UIViewController {
     @IBAction func registerPressed(_ sender: AnyObject) {
         
 
-        
+        SVProgressHUD.show()
         //TODO: Set up a new user on our Firbase database
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            if error != nil {
+                self.failureAlert()
+                print(error!)
+            }
+            else {
+                print("success")
+                SVProgressHUD.dismiss()
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
+  
         
-        
-
-        
-        
-    } 
+    }
+    func failureAlert(){
+        let alert = UIAlertController(title: "Registration failed", message: "connection error", preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: { (action) in
+            SVProgressHUD.dismiss()
+            self.dismiss(animated: true, completion: nil)
+        })
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
     
     
 }
